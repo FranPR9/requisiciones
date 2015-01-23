898ca3fc7610dd9b93544f07dd8ab8fc746c0774
kuamatzin <kuamatzin@gmail.com>
2015-01-23 14:01:10 -0600

    Migración hecha

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'requisiciones',
+			'username'  => 'root',
+			'password'  => '1123581321',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ app/database/migrations/2015_01_23_195608_create_requisiciones_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequisicionesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('requisiciones', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('id_dependencia')->unsigned();
+			$table->foreign('id_dependencia')->references('id')->on('dependencias');
+			$table->text('descripcion');
+			$table->integer('partida_presupuestal');
+			$table->string('codificacion', 300);
+			$table->double('presupuesto');
+			$table->integer('origen_recursos');
+			$table->integer('procedimiento_adjudicacion');
+			$table->string('tiempo_entrega', 500);
+			$table->string('lugar_entrega', 500);
+			$table->string('garantia', 200);
+			$table->string('asesor', 400);
+			$table->string('cargo_asesor', 200);
+			$table->string('email_asesor', 50);
+			$table->string('dias_pago', 300);
+			$table->text('observaciones');
+			$table->text('requisitos_tecnicos');
+			$table->text('requisitos_economicos');
+			$table->text('requisitos_informativos');
+			$table->text('condiciones_pago');
+			$table->text('datos_facturacion');
+			$table->timestamps();
+			//¿Qué papel tienen los asesores en el sistema? esos mismos serían usuarios?
+			//Deben saber que usuario hizo tal requisición
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('requisiciones');
+	}
+
+}
